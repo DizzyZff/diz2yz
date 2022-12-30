954f374f7df280f02cf6aa4e64e960384c696fc7
Desmond Zhu <hz1778@nyu.edu>
2022-12-29 21:44:26 -0500

    finish sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import logo from './myAvatar.PNG';
 import './App.css';
-import { Accordion, AccordionSummary, ExpandMoreIcon, Typography, AccordionDetails, Button } from '@mui/material';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+
+
 
 function App() {
   return (
@@ -12,7 +17,51 @@ function App() {
           </figure>
           <div className="App-sidebar-info-name">Desmond Zhu</div>
           <div className="App-sidebar-info-title">Software Engineer</div>
+          <button className="App-sidebar-info-more-button" onClick={
+            () => {
+              const contacts = document.querySelector('.App-sidebar-info-contacts');
+              const button = document.querySelector('.App-sidebar-info-more-button');
+              const collapseButton = document.querySelector('.App-sidebar-info-more-button-icon-collapse');
+              button.classList.toggle('hidden');
+              contacts.classList.toggle('hidden');
+              collapseButton.classList.toggle('hidden');
+              
+            }
+          }>
+            <ArrowDropDownIcon className="App-sidebar-info-more-button-icon"/>
+            </button>
+
+            <div className="App-sidebar-info-contacts hidden">
+              <div className="App-sidebar-info-contacts-item">
+                <GitHubIcon className="App-sidebar-info-contacts-item-icon"/>
+                <a href="www.github.com/dizzyZff" className="App-sidebar-info-contacts-item-link">Github</a>
+                </div>
+              <div className="App-sidebar-info-contacts-item">
+                <EmailIcon className="App-sidebar-info-contacts-item-icon" />
+                <a href="mailto:desmondzooooom@gmail.com" className="App-sidebar-info-contacts-item-link">Email</a>
+                </div>
+              <button className="App-sidebar-info-more-button" onClick={
+                () => {
+                  const contacts = document.querySelector('.App-sidebar-info-contacts');
+                  const button = document.querySelector('.App-sidebar-info-more-button');
+                  const collapseButton = document.querySelector('.App-sidebar-info-more-button-icon-collapse');
+                  button.classList.toggle('hidden');
+                  contacts.classList.toggle('hidden');  
+                  collapseButton.classList.toggle('hidden');
+                }
+              }>
+            <ArrowDropUpIcon className="App-sidebar-info-more-button-icon-collapse hidden"/>
+            </button>
+            </div>
+            
           </div>
+        </div>
+
+      <div className="App-content">
+        <div className="App-content-banner">
+
+        </div>     
+            
         </div>
     </div>
   );
